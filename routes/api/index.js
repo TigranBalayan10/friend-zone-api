4df02f9f9f8ebe219831a7e5f0a7c1cd8b0597ae
TigranBalayan10 <tigranbalayan10@gmail.com>
2022-04-22 13:33:40 -0700

    folder structure setup

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const commentRoutes = require('./thoughts-routes');
+const pizzaRoutes = require('./user-routes');
+
+router.use('/thoughts', commentRoutes);
+router.use('/users', pizzaRoutes);
+
+module.exports = router;
